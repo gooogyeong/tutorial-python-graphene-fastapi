8f847ce0f6d298ff65e080ea29296eb49b4b1b85
Min <minkyunglee@Minkyungs-MacBook-Air.local>
2024-09-18 17:09:15 +0200

    feat: configure server

--- server.py
+++ server.py
@@ -3,11 +3,9 @@ from starlette.routing import Route
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from schema import schema
 from starlette.responses import PlainTextResponse, JSONResponse
-
-# def index(request):
-#     return PlainTextResponse('Hello, world!')
 
 async def index(request):
     return JSONResponse({"message": "Hello, world"})
@@ -16,7 +14,19 @@ routes = [
     Route('/', index),
 ]
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = Starlette(routes=routes)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/graphql", GraphQLApp(schema=schema, on_get=make_graphiql_handler()))  # Graphiql IDE
 
